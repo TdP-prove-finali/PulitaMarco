b971d949b033e3e0574c4f6626721a296b199b51
marcopulita <67041362+marcopulita@users.noreply.github.com>
2020-09-01 22:42:09 +0200

    1 settembre

--- DroneDelivery/src/main/java/it/polito/tdp/dronedelivery/db/DeliveryDAO.java
+++ DroneDelivery/src/main/java/it/polito/tdp/dronedelivery/db/DeliveryDAO.java
@@ -5,31 +5,44 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import com.javadocmd.simplelatlng.LatLng;
+import com.javadocmd.simplelatlng.LatLngTool;
+import com.javadocmd.simplelatlng.util.LengthUnit;
 
+import it.polito.tdp.dronedelivery.model.Distance;
 import it.polito.tdp.dronedelivery.model.Shipment;
 
 public class DeliveryDAO {
 
 
-	public List<Shipment> getAllShipments() {
+	public List<Shipment> getAllShipments(String city, Map<Integer, Shipment> idMap) {
 
 		final String sql = "SELECT REQID, Address, City, State, Zip, Latitude, Longitude " + 
-		                   "FROM delivery where REQID = 23 ORDER BY City ASC";
+		                   "FROM delivery where City = ? ORDER BY City ASC";
 		List<Shipment> shipments = new ArrayList<Shipment>();
 
 		try {
 			Connection conn = DBConnect.getConnection();
 			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, city);
 			ResultSet rs = st.executeQuery();
 
 			while (rs.next()) {
+				
+				if(!idMap.containsKey(rs.getInt("REQID"))) {
+				
 				Shipment s = new Shipment(rs.getInt("REQID"), rs.getString("Address"),
 						rs.getString("City"), rs.getString("State"), rs.getInt("Zip"),
 						new LatLng(rs.getDouble("Latitude"), rs.getDouble("Longitude")));
 				shipments.add(s);
+				
+				idMap.put(s.getReqID(), s);
+				}
 			}
 
 			st.close();
@@ -42,7 +55,7 @@ public class DeliveryDAO {
 
 		return shipments;
 	}
-
+	
 	public Shipment getShipment(int reqID) {
 
 		final String sql = "SELECT REQID, Address, City, State, Zip, Latitude, Longitude " + 
@@ -70,38 +83,59 @@ public class DeliveryDAO {
 		return s;
 	}
 	
+	public List<Distance> getDistances(String city, Map<Integer, Shipment> idMap) {
+		
+		//Preparing the subset of objects to query. 
+		//This is not a user input, safe to insert directly in the query as map keys are mandatory as INT. 
+		//The query is length variable so not cached optimally and need to be rebuilt each time
+		//In this case building the query changing directly the string for the IN statement.
+		//Different is the case of "city" field. This is a string and need to be filtered.
+		Set<Integer> vertexes = idMap.keySet();
+		String inStr="";
+		Iterator<Integer> v = vertexes.iterator();
+		while(v.hasNext()) {
+			inStr = inStr+= v.next();
+			if(v.hasNext()) inStr = inStr+= ",";
+		}
+		//System.out.println(inStr);
+		// inStr = "319,320,322,323";
+		
+		
+		
+		String sql = "SELECT d1.REQID P1, d2.REQID P2, d1.city P1city, d1.address P1Address, " + 
+		                   "d2.address P2Address, d1.Latitude la1, d2.Latitude la2, d1.Longitude lo1, d2.Longitude lo2 " + 
+				           "FROM delivery d1, delivery d2 " +
+		                   "WHERE d1.city = ? AND d2.city = ? " +
+			               "AND d1.REQID < d2.REQID AND d1.REQID IN ("+inStr+") AND d2.REQID IN  ("+inStr+")"; 
+		
+		
+		List<Distance> distances = new ArrayList<Distance>();
 
-	
-	/*public List<Fermata> fermateSuccessive(Fermata fp, Map<Integer, Fermata> fermateIdMap) {
-		String sql = "SELECT DISTINCT id_stazA FROM connessione WHERE id_stazP = ?";
-		List<Fermata> result = new ArrayList<>();
 		try {
 			Connection conn = DBConnect.getConnection();
 			PreparedStatement st = conn.prepareStatement(sql);
-			st.setInt(1, fp.getIdFermata());
-			ResultSet res = st.executeQuery();
-			
-			######################################################
-			
-			  USE MAP TO REMOVE DUPLICATES LIKE IN METRO EXAMPLE
-			
-			######################################################
-			
-			while(res.next()) {
-				int id_fa = res.getInt("id_stazA"); //ID FERMATA
-				result.add(fermateIdMap.get(id_fa));
+			st.setString(1, city);
+			st.setString(2, city);
+			System.out.println(st);
+			ResultSet rs = st.executeQuery();
+
+			while (rs.next()) {
+				LatLng pc1 = new LatLng(rs.getDouble("la1"), rs.getDouble("lo1"));
+				LatLng pc2 = new LatLng(rs.getDouble("la2"), rs.getDouble("lo2"));
+				Double distP1P2 = LatLngTool.distance(pc1, pc2, LengthUnit.KILOMETER);
+				Distance d = new Distance(rs.getInt("P1"), rs.getInt("P2"), distP1P2);			                 
+				distances.add(d);
 			}
-			
-			
+
+			st.close();
 			conn.close();
-			
-			
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-		
-	} return result;
-		
-	}*/
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("DB Connection Error!");
+		}
+
+		return distances;
+	}
 
 }
--- DroneDelivery/src/main/java/it/polito/tdp/dronedelivery/db/TestDAO.java
+++ DroneDelivery/src/main/java/it/polito/tdp/dronedelivery/db/TestDAO.java
@@ -2,7 +2,6 @@ package it.polito.tdp.dronedelivery.db;
 
 import java.sql.Connection;
 
-
 import com.javadocmd.simplelatlng.LatLngTool;
 import com.javadocmd.simplelatlng.util.LengthUnit;
 
@@ -20,14 +19,19 @@ public class TestDAO {
 			
 			DeliveryDAO dao = new DeliveryDAO() ;
 			
+			
+			
 			Shipment s1 = dao.getShipment(330);
 			Shipment s2 = dao.getShipment(338);
 			
 			System.out.println(LatLngTool.distance(s1.getCoords(), s2.getCoords(), LengthUnit.KILOMETER));
 			
-			System.out.println(s1.toString()) ;
-			System.out.println(s2.toString()) ;
+			System.out.println(s1) ;
+			System.out.println(s2) ;
 
+			
+			
+			
 		} catch (Exception e) {
 			throw new RuntimeException("Connection Test FAILED", e);
 		}
